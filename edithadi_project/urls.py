ec299d0ddf96469cea6ab3b665b7cbd310d30713
hadibuxm <mahessarm@gmail.com>
2020-05-22 09:58:45 +0500

    v.1

--- edithadi_project/urls.py
+++ edithadi_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello.views import myview
-from todo.views import todoView
+from todo.views import todoView,addTodo,deleteTodo
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sayhello/',myview),
     path('todo/',todoView),
+    path('addTodo/',addTodo),
+    path('deleteTodo/<int:todo_id>/',deleteTodo)
 ]
